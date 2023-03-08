91e98604c23246af899dc5f132dd32bb85580250
hyq <3150981659@qq.com>
2023-03-08 12:49:18 +0800

    第一次

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,32 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHashHistory, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
+const routes = [{
+        path: "/",
+        name: "home",
+        component: HomeView,
+    },
+    {
+        path: "/about",
+        name: "about",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    },
+    //测试用的
+    // {
+    //     path: "/test",
+    //     name: "test",
+    //     component: () =>
+    //         import ("../views/TestView.vue")
+    // }
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
+    history: createWebHashHistory(),
+    routes,
 });
 
 export default router;
