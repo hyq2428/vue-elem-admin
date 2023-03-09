c45b7111921f3597f1153b73ea0248be5eed9a0c
hyq <3150981659@qq.com>
2023-03-09 16:37:16 +0800

    第二天 element手动按需导入有bug 和自动导入一样 需修复

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
 const ElementPlus = require('unplugin-element-plus/webpack').default;
 // const AutoImport = require('unplugin-auto-import/webpack');
 // const Components = require('unplugin-vue-components/webpack');
