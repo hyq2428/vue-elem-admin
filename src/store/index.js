3cae2a2b3c700a1dfa023f010498c0bc4b6472ef
hyq <3150981659@qq.com>
2023-03-31 13:30:08 +0800

    vuex状态守护

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,25 @@
 import { createStore } from "vuex";
 
+import { createApp } from "vue";
+import App from '.././App.vue';
+import Vuex from 'vuex';
+createApp(App).use(Vuex)
+
+// export default createStore({
+//   state: {},
+//   getters: {},
+//   mutations: {},
+//   actions: {},
+//   modules: {},
+// });
+
+// import Vue from 'vue';
+// import Vuex, { createStore } from 'vuex';
+
+import app from './modules/app';
+
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-});
+  modules:{
+    app
+  }
+})
--- src/views/console/Index.vue
+++ src/views/console/Index.vue
@@ -3,10 +3,21 @@
     
 </template>
 <script>
-    import { Avatar } from '@element-plus/icons-vue';
+//引入图标在本文件生效 局部引用
+    // import { Avatar } from '@element-plus/icons-vue';
+    // export default{
+    //     components:{
+    //         Avatar
+    //     }
+    // }
+    import {useStore} from 'vuex';
     export default{
-        components:{
-            Avatar
+        setup(){
+            const store = useStore();
+            const {state} = useStore();
+            const value = state.app.count
+            const value_store = store.app.count
+            return 
         }
     }
 </script>
