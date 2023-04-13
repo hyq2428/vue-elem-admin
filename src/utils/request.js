f7ccf837bd811293e17925d7036fc8c67980b3a8
hyq <3150981659@qq.com>
2023-04-13 09:25:15 +0800

    自动退出登录

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,17 +1,26 @@
 import axios from "axios";
+import {getToken,getUserName,delToken,delUserName} from "./cookies";
+import router from "@/router";
 
 const service = axios.create({
     baseURL: process.env.VUE_APP_API,
     timeout: 5000,
 })
-
+// 请求前
 service.interceptors.request.use(function(config) {
+    if (getToken()){
+        config.headers['Token'] = getToken()
+    }
+    if (getUserName()){
+        config.headers['Username'] = getUserName()
+    }
+    
     return config
 }, function(error) {
     console.log(111)
     return Promise.reject(error)
 })
-
+// 请求后
 service.interceptors.response.use(function(response) {
     const data = response.data;
     if (data.resCode === 0) {
@@ -28,6 +37,13 @@ service.interceptors.response.use(function(response) {
         ElMessage.error({
             message: errorData.message
         })
+    }
+    if(errorData.resCode==1010){
+        router.replace({
+            name:"Login"
+        })
+        delToken()
+        delUserName()
     }
     return Promise.reject(errorData)
 })
